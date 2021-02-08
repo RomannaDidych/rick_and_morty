cdcfad305a81021542e92859245ce2a5d0d7f443
unknown <romanna.d@gmail.com>
2021-02-08 22:54:55 +0200

    finised chracters page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect, NavLink} from 'react-router-dom';
 import Characters from './Components/Characters/Characters';
 import Episodes from './Components/Episodes/Episodes';
 import Locations from './Components/Locations/Locations';
@@ -16,15 +16,16 @@ function App() {
                   <NavLink className='app-link' to='/characters' activeClassName='active-link'>Characters</NavLink>
                   <NavLink className='app-link' to='/episodes' activeClassName='active-link'>Episodes</NavLink>
                   <NavLink className='app-link' to='/locations' activeClassName='active-link'>Locations</NavLink>
-                  <NavLink className='app-link' to='/WatchList' activeClassName='active-link'>My Watch List</NavLink>                 
+                  <NavLink className='app-link' to='/watchList' activeClassName='active-link'>My Watch List</NavLink>                 
                </div>               
       
                <Switch className='switch'>
                   <Route path='/characters' component= {Characters} exact/>
                   <Route path='/episodes' component= {Episodes} exact/>
                   <Route path='/locations' component= {Locations} />
-                  <Route path='/WatchList' component= {WatchList} exact/>                                    
-                  <Route path='/' component= {Characters} exact/>                 
+                  <Route path='/watchList' component= {WatchList} exact/>                                    
+                  <Route path='/' component= {Characters} exact/>
+                  <Redirect to="/characters" />                 
                 </Switch>
         </Router>
         </div>
--- src/Components/Characters/Characters.js
+++ src/Components/Characters/Characters.js
@@ -18,12 +18,28 @@ class Characters extends Component{
         this.setMaxAmount();               
     }
 
-	getChracters = async () =>{		
-		const arr = this.getNextArr()				
+	getChracters = async (e) =>{
+		let arr;
+		if (e.target.value === 'next')	{
+			if (this.currentNumber > this.maxAmount) {
+				return;
+			}
+			arr = this.getNextArr();
+			this.currentNumber += 10;			
+		} else {
+			if (this.currentNumber <= 0){
+				return;
+			}
+			arr = this.getPreviousArr();
+			this.currentNumber -= 10;
+		}	
+		//const arr = this.getNextArr()				
 		const response = await fetch(`${mainURL}${arr}`);		
 		const items = await response.json();
-		this.setState({characters: items})		
-		const newCurrent = this.currentNumber + 10;		
+		this.setState({characters: items});		
+		//const newCurrent = this.currentNumber + 10;
+		//this.currentNumber += 10;
+		console.log(this.currentNumber);		
 		return items;		
 	}
 
@@ -33,10 +49,10 @@ class Characters extends Component{
 		this.maxAmount = count.info.count;				
 	}
 
-	/*getPreviousIDarr = () => {
-		const arr = Array.from({length: this.getNextLength()}).map((arr, ind) => this.state.currentNumber + ind - 9);
+	getPreviousArr = () => {
+		const arr = Array.from({length: countOnPage}).map((arr, ind) => this.currentNumber + ind - 9);
 		return arr;
-	}*/
+	}
 
 	getNextArr = () => {
 		const condition = this.maxAmount - this.currentNumber;
@@ -52,8 +68,8 @@ class Characters extends Component{
 
 	render(){
 		
-		const data = this.getChracters;
-		console.log(data);		
+		//const data = this.getChracters();
+		//console.log(data);		
 		return (
 			<div className='container'>				
 				<h2 className='title'>Characters</h2>
@@ -61,8 +77,8 @@ class Characters extends Component{
 					{this.state.characters.map((hero) => <Card {...hero} key={hero.id} />)}
 				</div>
 				<div className= "buttons">
-					<button onClick={this.getChracters} value='previous'>previous characters</button>
-					<button onClick={this.getChracters} value='next'>next characters</button>
+					<button className= 'btn' onClick={this.getChracters} value='previous'>previous characters</button>
+					<button className= 'btn' onClick={this.getChracters} value='next'>next characters</button>
 				</div>
 			</div>
 			)
