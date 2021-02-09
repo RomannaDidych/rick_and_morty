fa2d2e3ac5308245d6e05ad73bce308319ff1cac
unknown <romanna.d@gmail.com>
2021-02-09 04:08:36 +0200

    added filters to characters page

--- src/Components/Characters/Characters.js
+++ src/Components/Characters/Characters.js
@@ -5,45 +5,57 @@ import './characters-style.scss';
 
 const mainURL = 'https://rickandmortyapi.com/api/character/';
 const countOnPage = 10;
-const characters = [];
+
 
 class Characters extends Component{
 	constructor() {
         super();
-        this.maxAmount = 0
-        this.currentNumber = 0        
+        this.maxAmount = 671
+        this.currentNumber = 0
+        this.allChracters = []
+        this.filteredCharacters = []
+        this.filterStatus = 'all'
+        this.filterGender = 'all'
+        this.filterSpecies = 'all'
+        this.filteredCharacters = []        
         this.state = {        	
         	characters: []
         }
-        this.setMaxAmount();               
+        //this.setMaxAmount();
+        this.getAllChracters();               
     }
 
-	getChracters = async (e) =>{
-		let arr;
+	getAllChracters = async (e) =>{
+		let arr =  Array.from({length:this.maxAmount}).map((arr, ind) =>  ind +1);  //this.currentNumber +					
+		const response = await fetch(`${mainURL}${arr}`);		
+		const items = await response.json();
+		this.allChracters = items;
+		this.filteredCharacters = items;		
+		const firstState = items.filter((obj, i) => i < countOnPage);
+		this.setState({characters: firstState});				
+	}
+
+	changeItemsOnPage = (e) => {
+		let newState;
 		if (e.target.value === 'next')	{
-			if (this.currentNumber > this.maxAmount) {
+			if (this.currentNumber >= this.filteredCharacters.length - 10) {
 				return;
 			}
-			arr = this.getNextArr();
-			this.currentNumber += 10;			
+			this.currentNumber += 10;
+			newState = this.filteredCharacters.slice(this.currentNumber, this.currentNumber + countOnPage );// change allChracters to filteredCharacters
+						
 		} else {
 			if (this.currentNumber <= 0){
 				return;
 			}
-			arr = this.getPreviousArr();
 			this.currentNumber -= 10;
-		}	
-		//const arr = this.getNextArr()				
-		const response = await fetch(`${mainURL}${arr}`);		
-		const items = await response.json();
-		this.setState({characters: items});		
-		//const newCurrent = this.currentNumber + 10;
-		//this.currentNumber += 10;
-		console.log(this.currentNumber);		
-		return items;		
-	}
+			newState = this.filteredCharacters.slice(this.currentNumber, this.currentNumber + countOnPage);
+			
+		};
+		this.setState({characters: newState});
+	};	
 
-	setMaxAmount = async () =>{
+	/*setMaxAmount = async () =>{
 		const response = await fetch(mainURL);
 		const count = await response.json();
 		this.maxAmount = count.info.count;				
@@ -63,22 +75,99 @@ class Characters extends Component{
 			arr =  Array.from({length:condition}).map((arr, ind) => this.currentNumber + ind +1);
 		}
 		return arr;
+	}*/
+
+	setFilteredCharacters = () => {
+		let current = [];
+		//if((this.filterStatus === 'all') || (this.filterGander === 'all') || (this.filterSpecies !== 'all')){}
+		this.filteredCharacters = this.allChracters;		
+		if(this.filterStatus !== 'all'){
+			current = this.filteredCharacters.filter((hero) => hero.status === this.filterStatus)
+			this.filteredCharacters = current;
+		};
+		if(this.filterGender !== 'all'){
+			current = this.filteredCharacters.filter((hero) => hero.gender === this.filterGender);
+			this.filteredCharacters = current;
+		};
+		if(this.filterSpecies !== 'all'){
+			current = this.filteredCharacters.filter((hero) => hero.species === this.filterSpecies)
+			this.filteredCharacters = current;
+		}
+	}
+
+	selectStatus = (e) => {
+		this.filterStatus = e.target.value;
+		//console.log(this.filterStatus);
+		this.setFilteredCharacters();
+		const currentState = this.filteredCharacters.filter((obj, i) => i < countOnPage);
+		this.setState({characters: currentState});
+		this.currentNumber = 0;
+	}
+
+	selectGender = (e) => {
+		this.filterGender = e.target.value;
+		//console.log(this.filterStatus);
+		this.setFilteredCharacters();
+		const currentState = this.filteredCharacters.filter((obj, i) => i < countOnPage);
+		this.setState({characters: currentState});
+		this.currentNumber = 0;
+	}
+
+	selectSpecies = (e) => {
+		this.filterSpecies = e.target.value;
+		//console.log(this.filterStatus);
+		this.setFilteredCharacters();
+		const currentState = this.filteredCharacters.filter((obj, i) => i < countOnPage);
+		this.setState({characters: currentState});
+		this.currentNumber = 0;
 	}
 
 
-	render(){
-		
-		//const data = this.getChracters();
-		//console.log(data);		
+	render(){				
 		return (
 			<div className='container'>				
-				<h2 className='title'>Characters</h2>
+								
+				<div className= "buttons">
+					<button className= 'btn' onClick={this.changeItemsOnPage} value='previous'>previous characters</button>
+					<div className='selectors'>
+						<div className='selector'>
+							<select name="status" id="status" onChange={this.selectStatus}>
+								<option value="all">all</option>
+								<option value="Alive">alive</option>
+								<option value="Dead">dead</option>
+								<option value="unknown">unknown</option>
+							</select>
+						</div>
+						<div className='selector'>
+							<select name="gender" id="status" onChange={this.selectGender}>
+								<option value="all">all</option>
+								<option value="Male">male</option>
+								<option value="Female">female</option>
+								<option value="Genderless">genderless</option>
+								<option value="unknown">unknown</option>
+							</select>
+						</div>
+						<div className='selector'>
+							<select name="species" id="status" onChange={this.selectSpecies}>
+								<option value="all">all</option>
+								<option value="Human">human</option>
+								<option value="Alien">alien</option>
+								<option value="Humanoid">humanoid</option>
+								<option value="Poopybutthole">poopybutthole</option>
+								<option value="unknown">unknown</option>
+								<option value="Mythological Creature">mythological creature</option>
+								<option value="Animal">animal</option>
+								<option value="Robot">robot</option>
+								<option value="Cronenberg">cronenberg</option>
+								<option value="Disease">disease</option>
+								<option value="Planet">planet</option>
+							</select>
+						</div>
+					</div>
+					<button className= 'btn' onClick={this.changeItemsOnPage} value='next'>next characters</button>
+				</div>
 				<div className= 'table'>
 					{this.state.characters.map((hero) => <Card {...hero} key={hero.id} />)}
-				</div>
-				<div className= "buttons">
-					<button className= 'btn' onClick={this.getChracters} value='previous'>previous characters</button>
-					<button className= 'btn' onClick={this.getChracters} value='next'>next characters</button>
 				</div>
 			</div>
 			)
@@ -91,4 +180,8 @@ export default Characters;
 //<button onClick={this.getChracters}>get Characters</button>
 //onClick={() =>{console.log(characters)}}
 //const response = await fetch(`https://rickandmortyapi.com/api/character/${Array.from({length: lengthArr}).map((arr, ind) => this.state.currentNumber + ind +1)}`);
-//const response = await fetch(`https://rickandmortyapi.com/api/character/${Array.from({length: 10}).map((arr, ind) => this.state.currentNumber + ind +1)}`);	
+//const response = await fetch(`https://rickandmortyapi.com/api/character/${Array.from({length: 10}).map((arr, ind) => this.state.currentNumber + ind +1)}`);
+//<h2 className='title'>Characters</h2>
+/*<div className= 'table'>
+					{this.state.characters.map((hero) => <Card {...hero} key={hero.id} />)}
+				</div>	*/
